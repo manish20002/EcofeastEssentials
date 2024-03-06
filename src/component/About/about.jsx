ca51e16b6257670178d608eee0d2e111337f9e78
manish20002 <manishthirani8d@gmail.com>
2024-03-06 12:58:09 +0530

    deploy

--- src/component/About/about.jsx
+++ src/component/About/about.jsx
@@ -41,7 +41,7 @@ export default function about() {
               <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
                 <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/201x201" />
                 <div className="flex-grow sm:pl-8">
-                  <h2 className="title-font font-medium text-lg text-gray-900">Payal</h2>
+                  <h2 className="title-font font-medium text-lg text-gray-900">Ritesh Asati</h2>
                   <h3 className="text-gray-500 mb-3">Designer</h3>
                   <p className="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
                   <div>
