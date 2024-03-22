ace9e475cf4423d35c4ba05ad661e72c734aacd3
manish20002 <manishthirani8d@gmail.com>
2024-03-22 09:33:14 +0530

    Updates

---
+++ precache-manifest.0f981310d492dc2ea8900d16e7a9f611.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a554c496f7decceb9b0",
+    "url": "/EcofeastEssentials/static/css/main.43c48e08.chunk.css"
+  },
+  {
+    "revision": "5a554c496f7decceb9b0",
+    "url": "/EcofeastEssentials/static/js/main.5a554c49.chunk.js"
+  },
+  {
+    "revision": "a55ae8da76b6c0c45aec",
+    "url": "/EcofeastEssentials/static/js/1.a55ae8da.chunk.js"
+  },
+  {
+    "revision": "cf36294442a174acca3a",
+    "url": "/EcofeastEssentials/static/js/2.cf362944.chunk.js"
+  },
+  {
+    "revision": "5f74f3f1d26e7b6f327d",
+    "url": "/EcofeastEssentials/static/js/runtime~main.5f74f3f1.js"
+  },
+  {
+    "revision": "d94aa77e717675e8072a74b1e886cba1",
+    "url": "/EcofeastEssentials/static/media/ecologo.d94aa77e.jpg"
+  },
+  {
+    "revision": "3efbfb59554ac1827c04bfb94f32a1a6",
+    "url": "/EcofeastEssentials/static/media/img2.3efbfb59.jpg"
+  },
+  {
+    "revision": "c4f993a3b5eb0ca0a166fda293b1ed54",
+    "url": "/EcofeastEssentials/static/media/img3.c4f993a3.jpg"
+  },
+  {
+    "revision": "1d258e3276a7ef44770d122b456d4cff",
+    "url": "/EcofeastEssentials/static/media/plates.1d258e32.jpg"
+  },
+  {
+    "revision": "a26298926a6abc88a77811aedf25fe83",
+    "url": "/EcofeastEssentials/static/media/bowls.a2629892.jpg"
+  },
+  {
+    "revision": "8389dab9ed83b5f079be85976712c010",
+    "url": "/EcofeastEssentials/static/media/cups1.8389dab9.jpg"
+  },
+  {
+    "revision": "dd56db1328415355883a2aa5ceb7a228",
+    "url": "/EcofeastEssentials/static/media/spoons1.dd56db13.jpg"
+  },
+  {
+    "revision": "9d38aba8cab854dc1d0b98d706adc16e",
+    "url": "/EcofeastEssentials/static/media/logorb.9d38aba8.png"
+  },
+  {
+    "revision": "39bdd56463d955a0df1c6dca527669ff",
+    "url": "/EcofeastEssentials/index.html"
+  }
+];
--- src/component/Nopage/nopg.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function nopg() {
-  return (
-    <div>404 error</div>
-  )
-}
